9b68fd127bfc199b301d5bda0680337f4c94b813
Moxley Stratton <moxley.stratton@gmail.com>
2020-10-11 16:16:46 -0700

    Stylize timeline points

--- src/components/PointsBar.jsx
+++ src/components/PointsBar.jsx
@@ -1,5 +1,29 @@
 import React from "react"
 
 export default function PointsBar() {
-  return <div style={{ height: "10px", backgroundColor: "#888" }} />
+  return (
+    <div
+      style={{
+        height: "10px",
+        backgroundColor: "#888",
+        marginBottom: "1em",
+        marginTop: "0.5em",
+        position: "relative",
+      }}
+    >
+      <div
+        style={{
+          width: "4px",
+          height: "19px",
+          backgroundColor: "yellow",
+          top: "-5px",
+          position: "absolute",
+          left: "30%",
+          cursor: "grabbing",
+        }}
+      >
+        &nbsp;
+      </div>
+    </div>
+  )
 }
