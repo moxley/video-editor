06778fe676138b61c185ae4bd7dbedbd9e318068
Moxley Stratton <moxley.stratton@gmail.com>
2020-10-13 20:21:51 -0700

    Start and edit markers reflect current start and edit points

--- src/components/PointsBar.jsx
+++ src/components/PointsBar.jsx
@@ -1,6 +1,51 @@
 import React from "react"
 
-export default function PointsBar() {
+export default function PointsBar(props) {
+  const { videoDuration, editPoint } = props
+  const { start, end } = editPoint.times
+
+  function startIndicator() {
+    if (!(videoDuration && start)) return null
+
+    const startPercent = (start / videoDuration) * 100
+
+    return (
+      <div
+        style={{
+          width: "4px",
+          height: "19px",
+          backgroundColor: "rgb(216, 216, 0)",
+          top: "-5px",
+          position: "absolute",
+          left: `${startPercent}%`,
+          cursor: "grabbing",
+        }}
+      >
+        &nbsp;
+      </div>
+    )
+  }
+
+  function endIndicator() {
+    if (!(videoDuration && end)) return null
+    const percent = (end / videoDuration) * 100
+    return (
+      <div
+        style={{
+          width: "4px",
+          height: "19px",
+          backgroundColor: "#0aa",
+          top: "-5px",
+          position: "absolute",
+          left: `${percent}%`,
+          cursor: "grabbing",
+        }}
+      >
+        &nbsp;
+      </div>
+    )
+  }
+
   return (
     <div
       style={{
@@ -13,17 +58,16 @@ export default function PointsBar() {
     >
       <div
         style={{
-          width: "4px",
-          height: "19px",
-          backgroundColor: "yellow",
-          top: "-5px",
-          position: "absolute",
-          left: "30%",
-          cursor: "grabbing",
+          height: "10px",
+          backgroundColor: "#aaa",
+          marginBottom: "1em",
+          marginTop: "0.5em",
+          position: "relative",
+          width: "20%",
         }}
-      >
-        &nbsp;
-      </div>
+      ></div>
+      {startIndicator()}
+      {endIndicator()}
     </div>
   )
 }
--- src/components/VideoEditor.jsx
+++ src/components/VideoEditor.jsx
@@ -8,12 +8,16 @@ const initialEditPoint = {
   arguments: {},
 }
 
+const videoRef = React.createRef()
+
 export default function VideoEditor() {
   const [edits, setEdits] = useState([])
   const [editPoint, setEditPoint] = useState(initialEditPoint)
+  const videoEl = videoRef.current
+  const videoDuration = videoEl && videoEl.duration
 
   function openControl() {
-    const video = document.getElementById("video")
+    const video = videoRef.current
     const times = { start: video.currentTime, end: null }
     setEditPoint({ ...editPoint, command: "scale", times })
   }
@@ -33,7 +37,12 @@ export default function VideoEditor() {
 
   return (
     <>
-      <video controls id="video" style={{ width: "100%", maxWidth: "960px" }}>
+      <video
+        controls
+        id="video"
+        style={{ width: "100%", maxWidth: "960px" }}
+        ref={videoRef}
+      >
         <source
           src="https://embedwistia-a.akamaihd.net/deliveries/aff5dbbb15cde4c917a1094efabe69a97ddb7d8b/Hackathon.mp4"
           type="video/mp4"
@@ -41,7 +50,11 @@ export default function VideoEditor() {
         Sorry, your browser doesn't support embedded videos.
       </video>
 
-      <PointsBar edits={edits} editPoint={editPoint} />
+      <PointsBar
+        edits={edits}
+        editPoint={editPoint}
+        videoDuration={videoDuration}
+      />
 
       <div>
         <button onClick={openControl}>Mark edit start</button>
